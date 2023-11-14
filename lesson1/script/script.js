0477b5dd035e5c5973e24d1ab13f2c1b8aee3532
Kostiantyn Kryvorotov <fatuglyaces@gmail.com>
2023-11-14 10:52:34 +0100

    14.11 10:52

---
+++ lesson1/script/script.js
@@ -0,0 +1,46 @@
+console.log('Hello world')
+
+let variableName = 'First variable'
+
+console.log(variableName)
+
+let num1 = 5
+num1 = 7
+console.log(num1)
+
+const pi = 3.14
+// pi = 5
+console.log(pi)
+
+let variable = 3
+variable = 'Text'
+
+//Типы данных
+
+//number
+
+let number1 = 23
+let number2 = 23.4
+
+//Базовые математические дейстивя
+
+console.log(5 + 3)
+console.log(5 - 3)
+console.log(6 / 4)
+console.log(4 ** 2) //возведение в степень
+console.log(11 % 2) //остаток от деления
+
+//Infinity
+//-Infinity
+//NaN - Not a Number
+
+console.log(3 / 0) //Infinity
+console.log(-3 / 0) //-Infinity
+console.log('not number' / 2) //NaN
+console.log(NaN + 1) //NaN
+
+//string
+
+//boolean
+
+//Object
