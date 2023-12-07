29a314b225ca76e874687dd04d92d081253dda85
Kostiantyn Kryvorotov <fatuglyaces@gmail.com>
2023-12-07 14:30:05 +0100

    07.12.2023 14:30

---
+++ Pro/Practice/lesson2/script.js
@@ -0,0 +1,29 @@
+let arr = [0, 1, 1, 1, 1, 0, 1, 0];
+let arrSorted = [];
+
+function sortedArray(array) {
+    let zeros = 0;
+    let ones = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === 0) {
+            arrSorted.unshift(array[i]);
+            zeros++;
+        }
+        if (array[i] === 1) {
+            arrSorted.push(array[i]);
+            ones++;
+        }
+    }
+    console.log('0 = ' + zeros, ', 1 = ' + ones);
+    return arrSorted;
+}
+
+console.log(sortedArray(arr));
+
+
+// for (let i = 0; i < zeros; i++) {
+//     array.push(0);
+// }
+// for (let i = 0; i < ones; i++) {
+//     array.push(1);
+// }
