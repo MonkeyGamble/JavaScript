de00a61ace055a28aedac14c7bbb7a2006c98c67
Kostiantyn Kryvorotov <fatuglyaces@gmail.com>
2023-12-18 11:46:59 +0100

    18.12.2023 11:46

--- HomeTasks/14.12/script.js
+++ HomeTasks/14.12/script.js
@@ -184,3 +184,32 @@ function nthEven(n) {
     if (n === 0) return 'undefined'
     return 2 * (n - 1)
 }
+
+//14.An NBA game runs 48 minutes (Four 12 minute quarters). Players do not typically play the full game,
+// subbing in and out as necessary. Your job is to extrapolate a player's points per game if they played the full 48 minutes.
+// Write a function that takes two arguments, ppg (points per game) and mpg (minutes per game)
+// and returns a straight extrapolation of ppg per 48 minutes rounded to the nearest tenth.
+// Return 0 if 0.An NBA game runs 48 minutes (Four 12 minute quarters).
+// Players do not typically play the full game, subbing in and out as necessary.
+// Your job is to extrapolate a player's points per game if they played the full 48 minutes.
+// Write a function that takes two arguments, ppg (points per game) and mpg (minutes per game)
+// and returns a straight extrapolation of ppg per 48 minutes rounded to the nearest tenth. Return 0 if 0.
+
+function pointsPer48(ppg, mpg) {
+    if (ppg === 0 || mpg === 0) return 0
+    let points = (ppg / mpg) * 48
+    return Math.round(points * 10) / 10
+}
+
+//15.Given a non-negative integer n, write a function to_binary/ToBinary which returns that number in a binary format.
+
+function toBinary(n) {
+    let binaryN = '';
+    if (n === 0) return '0'
+    for (let i = 0; n > 0; i++) {
+        binaryN = (n % 2) + binaryN
+        n = Math.floor(n / 2)
+    }
+    return Number(binaryN)
+}
+
